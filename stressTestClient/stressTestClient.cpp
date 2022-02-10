ecd8f8b4de4f20fd46c715ae752b661abb7d519e
Korakit Seemakhupt <korakit@virginia.edu>
2022-02-10 16:43:06 -0500

    First commit

---
+++ stressTestClient/stressTestClient.cpp
@@ -0,0 +1,161 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+#include "../socketHandler/socketHandler.h"
+#include <errno.h>
+#include <string.h>
+#include "stdlib.h"
+#include "stdio.h"
+#include <unordered_set>
+#include <vector>
+#include <string>
+#include <chrono>
+#include <algorithm>
+#include <sys/types.h>
+#include <unistd.h>
+#include <assert.h>
+// Constants
+
+// Enable per-request timing
+//#define TIMING
+
+#define REMOTE_ADDRESS "127.0.0.1"
+
+int downStreamUDPSock = 0;
+int numOps = 100000;
+int payloadSize = 128;
+char sendBuff[2048];
+char recvBuff[2048];
+int usePMSwitch = 1;
+int port_no = 0;
+uint32_t* timeArray;
+unordered_set <string> usedkeys_set;
+vector <string> usedkeys_vec;
+int dumpTiming = 0;
+
+
+void initializePayload(){
+    int i;
+    int* randomPayload_intPtr = (int*)sendBuff;
+    for(i=0;i<sizeof(sendBuff)/sizeof(int);i++){
+        randomPayload_intPtr[i] = rand();
+    }
+}
+
+int seqnoToKey(int seqno){
+    return seqno;
+}
+
+int runTest(){
+    char appBuff[5000];
+    //char pmSwitchBuff[5000];
+    
+    int ctr = 0;
+    int numWrite = 0;
+    auto start_time = chrono::high_resolution_clock::now();
+    auto lastReqEnd_time = start_time;
+    auto thisReqEnd_time = start_time;
+    struct pmswitchHeader* hds = (struct pmswitchHeader*)sendBuff;
+    while(ctr<numOps){
+        int ret;
+        ret = socketHandler_send_bytes(downStreamUDPSock, sendBuff, payloadSize);
+        assert(ret==payloadSize && "Send error");
+        ret = socketHandler_recv_bytes(downStreamUDPSock, recvBuff, sizeof(recvBuff)); 
+        assert(ret && "Recv error");
+        //Timing
+#ifdef TIMING
+        thisReqEnd_time = chrono::high_resolution_clock::now();
+        timeArray[ctr] = (uint32_t)chrono::duration_cast<chrono::microseconds>(thisReqEnd_time - lastReqEnd_time).count();
+        lastReqEnd_time = thisReqEnd_time;
+#endif
+        ctr++;
+    }
+    ofstream statFile;
+    
+
+    
+    auto end_time = chrono::high_resolution_clock::now();
+#ifndef TIMING
+    uint32_t totalTime = (uint32_t)chrono::duration_cast<chrono::microseconds>(end_time - start_time).count();
+    cout << "totalTime: " << totalTime << endl;
+    cout << "AvgTime: " << ((double)totalTime)/ctr << endl;
+    cerr << ctr << endl;
+#else
+    // timeArray
+    std::vector<uint32_t>timeVectorMicroFull(timeArray, &timeArray[ctr]);
+    std::vector<uint32_t>timeVectorMicro(&timeArray[ctr*10/100], &timeArray[ctr*95/100]);
+    int64_t totalTimeMicro = 0;
+    int dataPtsCount = (ctr*95/100) - (ctr*10/100);
+    for(int k=0;k<timeVectorMicro.size();k++){
+        totalTimeMicro += timeVectorMicro[k];
+    }
+    cout << "totalTime: " << totalTimeMicro << endl;
+    cout << "AvgTime: " << (double)totalTimeMicro/dataPtsCount << endl;
+    // std::sort(timeVectorMicro.begin(), timeVectorMicro.end());
+    // cout << "LowestTime " << timeVectorMicro[0] << ", Longest Time " << timeVectorMicro.back() << endl; 
+    // cout << "P95: " << timeVectorMicro[timeVectorMicro.size()*95/100] << ", P99: " << timeVectorMicro[timeVectorMicro.size()*99/100] << endl;
+    if(dumpTiming){
+        ofstream dumpFile;
+        dumpFile.open ("distribution_pmSwitch_size_" + to_string(payloadSize) + ".txt", ostream::trunc);
+        for(int itr=0;itr<timeVectorMicro.size();itr++){
+            dumpFile <<timeVectorMicro[itr] << endl;
+        }
+        dumpFile.close();
+        return 0;
+    }
+#endif
+    return 0;
+}
+
+
+
+
+int main(int argc, char* argv[]){
+    // Parse parameters
+    const char* useErrorMsg = "Use: ./stressTestClient numOps payloadSize port dumpTiming\n";
+    if(argc>1){
+        if(argv[1][0]>'9'||argv[1][0]<'0'){
+            cerr << useErrorMsg << endl;
+            exit(1);
+        }
+        numOps = atoi(argv[1]);
+    }
+    if(argc>2){
+        if(argv[2][0]>'9'||argv[2][0]<'0'){
+            cerr << useErrorMsg << endl;
+            exit(1);
+        }
+        payloadSize = atoi(argv[2]);
+    }
+    if(argc>3){
+        if(argv[3][0]=='-'){
+            ;
+        }else{
+            if(argv[3][0]>'9'||argv[3][0]<'0'){
+                cerr << useErrorMsg << endl;
+                exit(1);
+            }else{
+                port_no = atoi(argv[3]);
+            }
+        }
+    }
+    if(argc>4){
+        if(argv[4][0]>'9'||argv[4][0]<'0'){
+            cerr << useErrorMsg << endl;
+            exit(1);
+        }
+        dumpTiming = atoi(argv[4]);
+    }
+    assert(((port_no>0) && (port_no<65536)) && "port No error");
+    // Prepare space for timing.
+#ifdef TIMING
+    timeArray = (uint32_t*)malloc(numOps*sizeof(uint32_t));
+#endif
+
+    // Initialize payload.
+    initializePayload();
+    downStreamUDPSock = socketHandler_connect(REMOTE_ADDRESS, port_no, DATAGRAM, BLOCKING);
+    //cerr << "Initialized" << endl;
+    runTest();
+
+}
---
+++ stressTestServer/stressTestServer.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <map>
+using namespace std;
+#include "../socketHandler/socketHandler.h"
+
+
+
+
+int main(int argc, char* argv[]){
+    int serverPort = 0;
+    const char* useErrorMsg = "Use: ./redisServerAdaptor PMSwitch_port\n";
+    if(argc>1){
+        if(argv[1][0]>'9'||argv[1][0]<'0'){
+            cerr << useErrorMsg << endl;
+            exit(1);
+        }
+        serverPort = atoi(argv[1]);
+    }
+
+    // UDP socket for PMSwitch downstream;
+    // Temp implementation, do not use this, it's shitty.
+    int serverSock = socketHandler_listen(serverPort, DATAGRAM, BLOCKING);
+    int ret=0;
+    char recvBuff[2048];
+    char sendBuff[2048];
+    int seqNumber = 0;
+    while(1){
+        struct sockaddr_in addressStruct;
+        size_t addr_struct_Size = sizeof(addressStruct);
+        size_t recvSize = 0;
+        size_t sendSize = 0;
+        ret = socketHandler_recv_bytes_from(serverSock, recvBuff, sizeof(recvBuff), &addressStruct, &addr_struct_Size);
+        if(ret==0){
+            cerr << "Socket closed, exiting";
+            exit(0);
+        }
+        recvSize = ret;
+        int port = ntohs(addressStruct.sin_port);
+        char src_ip[40];
+        inet_ntop(AF_INET, (void*)&addressStruct.sin_addr.s_addr, src_ip, sizeof(src_ip));
+        socketHandler_send_bytes_to(serverSock, sendBuff, sendSize, (const char*)src_ip, port);
+    }
+
+
+}
